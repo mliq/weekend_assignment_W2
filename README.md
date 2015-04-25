2f228f1cd31970ac7e690b07f5e5affd807b446c
mliq <liquori@gmail.com>
2015-04-24 23:41:28 -0500

    Groups by number works, but last one is undefined

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Prime Week 2 Weekend Assignment
 
 Create an app that sorts our class of 20 into groups of 2-10 each.
+
+Ideas:
+1. Fade in the teams.
+2. Use class instead of text to identify, or better yet .data().
+3. More visuals (try out jQuery UI?).
--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-var i = 0;
-var color;
-var colorDiv;
+var i = 0, groupSize, shuffledArray, groupArray, groupCounter;
+var namesArray = ["Erik", "Alicia", "Brian", "Casie", "Chelsea", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Michael", "Luke", "Mary", "Aaron", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince"];
+
+function shuffle(array){
+    // Fisher-Yates shuffle courtesy of: http://bost.ocks.org/mike/shuffle/
+
+    // initializes m as our array iterator, t as a temporary storage for the 'card' we are taking out of the 'deck', i will be a random index where we will place our 'card'.
+    var m = array.length, t, i;
+
+    //shuffle array.length times
+    while (m) {
+        // Pick a random number 0-1, multiply by remaining element count, decrement remaining element count, round down to whole integer. Essentially, choose a random place to put the current array index m.
+        i = Math.floor(Math.random() * m--);
+
+        // Store current array index m value in t.
+        t = array[m];
+        // Store the randomly chosen index value into the currently iterating index space.
+        array[m] = array[i];
+        // store the original currently iterating index value into the randomly chosen index space.
+        array[i] = t;
+    }
+    return array;
+}
 
 $(document).ready(function(){
 
-    $('#generate').on('click',function(){
-        i++;
-        $('body').append("<div class='line'>Line #"+i+
-            "<div class='changeColor'>Change Color</div><div class='remove'>Remove</div></div>");
+    $('.generate').on('click',function(){
+        // Get groupsize from button text.
+        groupSize=$(this).text();
+        // shuffle names array;
+        shuffledArray = shuffle(namesArray);
 
-        $('.changeColor').last().on('click',function(){
+        // Empty array one by one, sorting into groupsize groups.
+        groupArray = [];
 
-            colorDiv = $(this).parents('.line');
-            color = colorDiv.css('background-color');
+        groupCounter=0;
 
-            //alert(color);
+        while(shuffledArray.length > 0) {
+            groupArray[groupCounter] = [];
 
-            if(color == 'rgb(255, 69, 0)') {
-                colorDiv.css('background-color', 'white');
+            for (i = 0; i < groupSize; i++){
+                groupArray[groupCounter].push(shuffledArray.pop());
             }
-            else {
-                colorDiv.css('background-color', 'orangered');
-            };
-        });
-
-        $('.remove').last().on('click',function(){
-            $(this).closest('.line').remove();
-        });
+            groupCounter++;
+        }
+        console.log(groupArray);
     });
+
 
 
 
